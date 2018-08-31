c9abe7358e7d8d436ac259e2799ae0150b49fe19
Jordan Rand <jorrand@gmail.com>
2018-08-31 15:06:00 -0600

    New, Show, Delete

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,43 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+
+    if @animal.save
+      render 'show'
+    else
+      render 'index'
+    end
+  end
+
+  def update
+    @animal = Animal.update(animal_params)
+
+    redirect_to animals_path
+  end
+
+  def destroy
+    Animal.find(params[:id]).destroy
+    redirect_to animals_path
+  end
+
+  private
+    def animal_params
+      params.require(:animal).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
+    end
+end
